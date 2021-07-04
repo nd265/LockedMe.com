39b6a4ae2d1e827ad99c44bf1070398aa60b935b
nd265 <navyad265@gmail.com>
2021-07-04 18:06:14 +0530

    Task 1 and 3 completed

--- src/com/lockers/lockedme/Main.java
+++ src/com/lockers/lockedme/Main.java
@@ -3,6 +3,9 @@ package com.lockers.lockedme;
 import com.lockers.lockedme.util.Constants;
 import com.lockers.lockedme.util.Util;
 
+import java.io.File;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -50,19 +53,97 @@ public class Main {
             case 1: displayFilesAndFolders();
                     break;
 
-                    
-
+            case 2: performFileOperation();
+                    break;
+            case 3: shutdownApplication();
+                    break;
             default:Util.displayMessage(Constants.INVALID_INPUT+", select a valid option");
                     askUserForInput();
                     break;
+        }
+        askUserForInput();
+    }
+
+    private static void performFileOperation()
+    {
+        for(String option: Constants.FILE_OPTIONS)
+        {
+            Util.displayMessage(option);
+        }
+
+        try
+        {
+            int option = Util.getIntegerUserInput(Constants.OPTION_SELECT_MESSAGE);
+
+            if(option<=0 || option > Constants.OPTIONS.length)
+            {
+                throw new Exception(Constants.INVALID_INPUT);
+            }
+
+            performFileAction(option);
 
         }
+        catch (Exception e)
+        {
+            Util.displayMessage(Constants.INVALID_INPUT+", select a valid option");
+            askUserForInput();
+        }
+    }
+
+    private static void performFileAction(int action)
+    {
+
+    }
+
+
+    private static void shutdownApplication()
+    {
+        Util.displayMessage(Constants.EXIT_MESSAGE);
+        System.exit(0);
     }
 
     private static void displayFilesAndFolders()
     {
 
+        File directoryPath = new File(navigateToWorkingDirectory());
+        String contents[] = directoryPath.list();
+
+        if(contents==null)
+        {
+            Util.displayMessage(Constants.INVALID_INPUT+" , you entered invalid path");
+        }
+        else if(contents.length==0)
+        {
+            Util.displayMessage(Constants.EMPTY_DIRECTORY);
+        }
+        else
+        {
+            Util.displayMessage(Constants.DISPLAY_MESSAGE);
+
+            Arrays.sort(contents);
+
+            for(int i=0; i<contents.length; i++) {
+                Util.displayMessage(contents[i]);
+            }
+        }
+        Util.displayMessage("\n");
     }
+
+    private static String navigateToWorkingDirectory()
+    {
+        try
+        {
+            String workingDirectory = Util.getStringUserInput(Constants.ASK_WORKING_DIRECTORY+", ex:-"+System.getProperty("user.dir"));
+            return workingDirectory.isEmpty()? System.getProperty("user.dir"): workingDirectory;
+        }
+        catch (Exception e)
+        {
+            Util.displayMessage(Constants.SERVER_ERROR);
+            return navigateToWorkingDirectory();
+        }
+
+    }
+
 
 
     public static void getUserDetails()
--- src/com/lockers/lockedme/util/Constants.java
+++ src/com/lockers/lockedme/util/Constants.java
@@ -9,11 +9,20 @@ public abstract class Constants {
     public static final String INVALID_INPUT="Sorry the input you entered is invalid";
     public static final String SERVER_ERROR="Error processing input , please try again";
 
+    public static final String ASK_WORKING_DIRECTORY="Please enter path of the directory, leave blank for current";
+    public static final String EMPTY_DIRECTORY ="Empty directory ";
+    public static final String DISPLAY_MESSAGE= "List of files and directories in the specified directory:";
+    public static final String EXIT_MESSAGE="Exiting application..";
 
     public static final String[] OPTIONS = new String[]{
-      "1. Get all the files in folder",
-      "2. Remove files"
-    };
+            "1. Get all the files in folder",
+            "2. Perform file operations",
+            "3. Exit"};
+
+    public static final String[] FILE_OPTIONS = new String[]{
+            "1. Get all the files in folder",
+            "2. Perform file operations",
+            "3. Exit"};
 
     public static final String OPTION_SELECT_MESSAGE= "Please select the number corresponding to the action you want  to perform";
 }
