e55a7691e52b0b48a06829688bbb776c5b19eac5
nd265 <navyad265@gmail.com>
2021-07-05 17:07:01 +0530

    Comments added

--- src/com/lockers/lockedme/Main.java
+++ src/com/lockers/lockedme/Main.java
@@ -5,25 +5,27 @@ import com.lockers.lockedme.util.Util;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.AccessDeniedException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
 
     public static void main(String[] args) {
 
+        // Create the enum maps to map user input to appropriate enum
         createUserInputEnumMapping();
+        // Greet the user !
         Util.displayMessage(Constants.USER_WELCOME_MESSAGE);
+        // Display application details
         Util.displayMessage(Constants.LOCKED_ME_DETAILS);
+        // Get the user name might be redundant
         getUserDetails();
+        // Provide options to the user to use the application
         askUserForInput();
 
     }
 
+    // Created a mapping to map numbers to human readable enum for better code understanding
     private static void createUserInputEnumMapping()
     {
         Util.mainContextOptionHashMap = new HashMap<>();
@@ -40,10 +42,13 @@ public class Main {
 
     }
 
+    // Show th context menu to the user
     private static void askUserForInput()
     {
+
         Util.displayMessage(Constants.OPTION_MENU);
 
+        // Populate all the options in command line
         for(String option: Constants.OPTIONS)
         {
             Util.displayMessage(option);
@@ -53,22 +58,25 @@ public class Main {
         {
             int option = Util.getIntegerUserInput(Constants.OPTION_SELECT_MESSAGE);
 
+            // throw exception if invalid input
             if(option<=0 || option > Constants.OPTIONS.length)
             {
                 throw new Exception(Constants.INVALID_INPUT);
             }
-
+            // Get the corresponding enum and perform the action
             performAction(Util.mainContextOptionHashMap.get(option));
 
         }
         catch (Exception e)
         {
+            // If user inputs invalid Integer option
             Util.displayMessage(Constants.INVALID_INPUT+", select a valid option");
             askUserForInput();
         }
 
     }
 
+    // Performs the operation based on the option selected in the context menu
     private static void performAction(Util.MainContextOption option)
     {
         switch (option)
@@ -84,11 +92,13 @@ public class Main {
                     break;
         }
         Util.displayMessage(Constants.TASK_EXECUTION_OVER);
+        // Instead of exiting the application we ask user if we want to continue using the application
         askUserForInput();
     }
 
     private static void performFileOperation()
     {
+        // Display all file related operations
         for(String option: Constants.FILE_OPTIONS)
         {
             Util.displayMessage(option);
@@ -98,20 +108,24 @@ public class Main {
         {
             int option = Util.getIntegerUserInput(Constants.OPTION_SELECT_MESSAGE);
 
+            // Throw exception if invalid integer input
             if(option<=0 || option > Constants.FILE_OPTIONS.length)
             {
                 throw new Exception(Constants.INVALID_INPUT);
             }
+            // Special case where we don't require the working directory and user just wants to go back to main context
             else if(Util.fileManipulationOptionHashMap.get(option)==Util.FileManipulationOption.BACK_TO_MAIN_CONTEXT)
             {
                 askUserForInput();
             }
+            // Any valid option perform the corresponding file manipulation action
             else
                 performFileAction(Util.fileManipulationOptionHashMap.get(option));
 
         }
         catch (Exception e)
         {
+            // Handles invalid user option selected
             Util.displayMessage(Constants.INVALID_INPUT+", select a valid option");
             performFileOperation();
         }
@@ -119,11 +133,11 @@ public class Main {
 
     private static void performFileAction(Util.FileManipulationOption action)
     {
-
-
-        File directoryPath = new File(navigateToWorkingDirectory());
+        // Gets the root directory from the user if left blank assumes the current working directory
+        File directoryPath = new File(getRootDirectory());
         String contents[] = directoryPath.list();
 
+        // Check to ensure that the path entered is indeed valid
         if(contents==null)
         {
             Util.displayMessage(Constants.INVALID_INPUT+" , you entered an invalid path");
@@ -131,6 +145,7 @@ public class Main {
         }
         else
         {
+            // All good and based on the action the functions are invoked
             switch (action)
             {
                 case CREATE_FILE: createNewFile(directoryPath);
@@ -139,8 +154,6 @@ public class Main {
                         break;
                 case SEARCH_FILE: searchForFileName(directoryPath);
                         break;
-
-
                 default: Util.displayMessage(Constants.INVALID_INPUT);
                         break;
             }
@@ -149,11 +162,13 @@ public class Main {
 
     }
 
+    // Creates a new file in the directory provided by the user with the name they input
     private static void createNewFile(File directory) {
+
+        // Get the file name
         String filename = getFileName();
-
         try {
-
+           // Create the file object and tries to create if success returns true if failed file name might already exist
            if(!new File(directory.getAbsolutePath(),filename).createNewFile())
            {
                throw new Exception("Duplicate resource");
@@ -161,10 +176,12 @@ public class Main {
            else
                Util.displayMessage(Constants.FILE_CREATED);
         }
+        // If file creation is not allowed to normal user we need to run the program in admin mode
         catch (IOException e)
         {
             Util.displayMessage(Constants.ACCESS_DENIED_EXCEPTION);
         }
+        // Any other exception is handled here
         catch (Exception e)
         {
             Util.displayMessage(Constants.SERVER_ERROR+" , unable to create file make sure file doesn't exist");
@@ -172,24 +189,30 @@ public class Main {
 
     }
 
+    // Deletes the files in the directory provided matching the file name they input
     private static void deleteFileFromDirectory(File directory)
     {
-
+        // Gets the file name to be deleted
         String filename = getFileName();
         try {
-
+            // We delete the file only if it's a exact match we might have files with same name and diff extension
             if(!new File(directory.getAbsolutePath(),filename).delete())
             {
+                // If delete operation is unsuccessful the file might not be present and we notify the user in that case
                 Util.displayMessage(Constants.FILE_NOT_FOUND);
             }
+            // Successful operation
             else
                 Util.displayMessage(Constants.FILE_DELETED);
         } catch (Exception e)
         {
+            // Any other issues that might occur during the deletion event
             Util.displayMessage(Constants.SERVER_ERROR);
         }
     }
 
+    // Search for a file name in the directory provided by the user . The user might not remember the
+    // exact file name and so we show all the file names with the prefix that user provides
     private static void searchForFileName(File directory)
     {
         String filename = getFileName();
@@ -206,14 +229,17 @@ public class Main {
 
         for(File file : contents)
         {
+            // Since we want to show only the files and not the directories which start with the name user provides
+            // Linear search
             if(file.isFile() && file.getName().startsWith(filename))
             {
                 macthingFiles.add(file.getName());
             }
         }
-
         Util.displayMessage(Constants.DISPLAY_FILE_LIST_MESSAGE);
-
+        // We want to display the files in the ascending order
+        Collections.sort(macthingFiles);
+        // Display all the files one by one
         for(String file: macthingFiles)
         {
             Util.displayMessage(file);
@@ -221,6 +247,7 @@ public class Main {
 
     }
 
+    // Helper function to ask user for the file name
     private static String getFileName()
     {
         try {
@@ -233,24 +260,28 @@ public class Main {
         }
     }
 
-
+    // Shuts the program with exit code 0 meaning normal termination
     private static void shutdownApplication()
     {
         Util.displayMessage(Constants.EXIT_MESSAGE);
         System.exit(0);
     }
 
+    // Takes the working directory and displays everything inside that folder in ascending order
     private static void displayFilesAndFolders()
     {
 
-        File directoryPath = new File(navigateToWorkingDirectory());
+        File directoryPath = new File(getRootDirectory());
         String contents[] = directoryPath.list();
 
+        // If path is invalid user must have made a typo and we want to ask if they want to retry ,
+        // if no retry we send them back to main context
         if(contents==null)
         {
             Util.displayMessage(Constants.INVALID_INPUT+" , you entered an invalid path");
             askForRetryPathName();
         }
+        // If the directory is empty we simply inform the user of the same
         else if(contents.length==0)
         {
             Util.displayMessage(Constants.EMPTY_DIRECTORY);
@@ -258,7 +289,7 @@ public class Main {
         else
         {
             Util.displayMessage(Constants.DISPLAY_LIST_MESSAGE);
-
+            // We sort the files and directories according to their name and display them
             Arrays.sort(contents);
 
             for(int i=0; i<contents.length; i++) {
@@ -268,6 +299,7 @@ public class Main {
         Util.displayMessage("\n");
     }
 
+    // Retry to check if user wants to retry the input again
     private static void askForRetryPathName()
     {
         try {
@@ -282,14 +314,15 @@ public class Main {
         }
         catch (Exception e)
         {
+            // Recursion we basically want to ask the user again shouldn't be an issue
             askForRetryPathName();
         }
 
 
     }
 
-
-    private static String navigateToWorkingDirectory()
+    // Gets the root directory where the user wants to perform their operation by default returns current working dir
+    private static String getRootDirectory()
     {
         try
         {
@@ -299,23 +332,24 @@ public class Main {
         catch (Exception e)
         {
             Util.displayMessage(Constants.SERVER_ERROR);
-            return navigateToWorkingDirectory();
+            return getRootDirectory();
         }
 
     }
 
-
-
+    // Gets user input and greets them . Can we worked upon or removed
     public static void getUserDetails()
     {
         try
         {
             String username = Util.getStringUserInput(Constants.QUESTION_USER_NAME);
 
+            // Perform a check on the input user based on regex since names can have only alphabets
             if(Util.isStringOnlyAlphabet(username))
             {
                 displayUserInformation(username);
             }
+            // Invalid user input and ask them again
             else
             {
                 Util.displayMessage(Constants.INVALID_INPUT + ", name should only contain alphabets");
@@ -330,6 +364,7 @@ public class Main {
 
     }
 
+    // Greets the user !
     private static void displayUserInformation(String username)
     {
         StringBuilder userInformation = new StringBuilder();
--- src/com/lockers/lockedme/util/Util.java
+++ src/com/lockers/lockedme/util/Util.java
@@ -7,12 +7,13 @@ import java.util.HashMap;
 
 public abstract class Util {
 
+    // Maps 1,2,3 to Code readable enum
     public enum MainContextOption{
         DISPLAY_FILES_AND_FOLDERS,
         FILE_MANIPULATION,
         EXIT_APPLICATION
     }
-
+    // Maps 1,2,3 to Code readable enum
     public enum FileManipulationOption{
         CREATE_FILE,
         DELETE_FILE,
@@ -20,17 +21,22 @@ public abstract class Util {
         BACK_TO_MAIN_CONTEXT
     }
 
+    // Map containing mapping of 1,2,3 to enum value
     public static HashMap<Integer, MainContextOption> mainContextOptionHashMap;
+    // Map containing mapping of 1,2,3 to enum value
     public static HashMap<Integer, FileManipulationOption> fileManipulationOptionHashMap;
 
+    // Gets integer input from user
     public static int getIntegerUserInput(String message) throws IOException {
         return Integer.parseInt(getUserInput(message));
     }
 
+    // Gets string input from user
     public static String getStringUserInput(String message) throws IOException {
         return getUserInput(message);
     }
 
+    // wrapper function to get user input in string format
     private static String getUserInput(String message) throws IOException {
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
@@ -38,11 +44,13 @@ public abstract class Util {
         return bufferedReader.readLine();
     }
 
+    // Utility function to display message
     public static void displayMessage(String message)
     {
         System.out.println(message);
     }
 
+    // regex to check the user name contains only alphabet
     public static boolean isStringOnlyAlphabet(String str)
     {
         return ((!str.equals(""))
